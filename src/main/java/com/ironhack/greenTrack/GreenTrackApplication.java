0275b44ba79e0fa916b11d2b14b8c170dcc4e317
peterParker79 <trypeterparker@gmail.com>
2025-05-06 17:36:23 +0200

    initialized project

---
+++ src/main/java/com/ironhack/greenTrack/GreenTrackApplication.java
@@ -0,0 +1,13 @@
+package com.ironhack.greenTrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenTrackApplication.class, args);
+	}
+
+}
