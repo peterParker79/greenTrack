1b1c8e516ad4e341a6a9d49065233482cbb73d7e
peterParker79 <trypeterparker@gmail.com>
2025-05-07 19:16:03 +0200

    Token Role generate test ok and added JsonCreator-toJson

--- src/main/java/com/ironhack/greenTrack/models/ERole.java
+++ src/main/java/com/ironhack/greenTrack/models/ERole.java
@@ -1,7 +1,21 @@
 package com.ironhack.greenTrack.models;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum ERole {
     ROLE_USER,
-    ROLE_ADMIN
+    ROLE_ADMIN;
+
+
+    @JsonCreator //deserializar
+    public static ERole fromString(String valueFromBody) {
+        return ERole.valueOf(valueFromBody.toUpperCase());
+    }
+
+    @JsonValue// serializar
+    public String toJson(){
+        return this.name().toLowerCase();
+    }
 
 }
